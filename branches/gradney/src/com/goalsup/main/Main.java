451d791e69c4c8d4062c0d1d20c966489f4a6896
gradney@gmail.com <gradney@gmail.com@d7fe4e90-87c4-f9b2-f6b7-09efb2fa5401>
2011-05-12 01:32:08 +0000

    Adding a quick dialog between our application and user. Asks simply for email and password, then displays a confirmation message. Still need test case though, which should be first before code is actually written. Warming up... :)

--- branches/gradney/src/com/goalsup/main/Main.java
+++ branches/gradney/src/com/goalsup/main/Main.java
@@ -1,15 +1,30 @@
 package com.goalsup.main;
 
+// Java extension packages
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	// main method starting the Java application
 	public static void main( String args[] ){
 		
 		// declare, initialize local variables
-		String strName = "GoalsUp";		// general local Name for within main
+		String strApplicationName = "GoalsUp!";		// general local Name for within main
+		String strEmail;
+		String strPassword;
+		String strConfirmationMessage;
 		
-		/** Simple Hello World to test our google svn. */
-		System.out.println("Hello World, " + strName + " is in the house!");
+		// Ask user for Email Address, Password
+		strEmail = JOptionPane.showInputDialog("Hello, please tell me your email address: ");
+		strPassword = JOptionPane.showInputDialog("Okay, please tell me your password to access your account: ");
+
+		// Build String with user's information
+		strConfirmationMessage = "Welcome to " + strApplicationName + " " + strEmail + ". We hope you're performing well!";
+		
+		// Display confirmation message to user
+		JOptionPane.showMessageDialog(null, strConfirmationMessage, "GoalsUp! - going above and beyond schooling!", JOptionPane.PLAIN_MESSAGE);
+
+		System.exit(0);		// terminate application
 		
 	}// end main()
 	

59f7d0035041dc8c9fe1d4a9b0be32db9540a9f8
gradney@gmail.com <gradney@gmail.com@d7fe4e90-87c4-f9b2-f6b7-09efb2fa5401>
2011-05-13 16:31:06 +0000

    Change the application into an applet for now (since Java applet open up in browsers). Still have a long way to go in actually implementing the feature: Sign-In.

--- branches/gradney/src/com/goalsup/main/Main.java
+++ branches/gradney/src/com/goalsup/main/Main.java
@@ -1,31 +1,69 @@
 package com.goalsup.main;
 
+// Java core packages
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 // Java extension packages
+import javax.swing.JApplet;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
-public class Main {
+public class Main extends JApplet implements ActionListener {
+	
+	// declare global (instance) variables
+	String g_strApplicationName = "GoalsUp!";		// initialize the value of the Application Name
+	JLabel g_lblEmail, g_lblPassword;
+	JTextField g_fldEmail;
+	JPasswordField g_fldPassword;
+	JButton g_btnSignIn;
+	
+	// initializing the applet (for browser use, which will eventually become better UI)
+	public void init(){
 
-	// main method starting the Java application
-	public static void main( String args[] ){
+		Container appContainer = getContentPane();
+		appContainer.setLayout( new FlowLayout() );
 		
-		// declare, initialize local variables
-		String strApplicationName = "GoalsUp!";		// general local Name for within main
-		String strEmail;
-		String strPassword;
-		String strConfirmationMessage;
+		// create the Email label, text field and add both to applet
+		g_lblEmail = new JLabel( "Email: " );
+		g_fldEmail = new JTextField( 20 );
+		appContainer.add( g_lblEmail );
+		appContainer.add( g_fldEmail );
 		
-		// Ask user for Email Address, Password
-		strEmail = JOptionPane.showInputDialog("Hello, please tell me your email address: ");
-		strPassword = JOptionPane.showInputDialog("Okay, please tell me your password to access your account: ");
+		// create the Password label, password field and add both to applet
+		g_lblPassword = new JLabel( "Password: " );
+		g_fldPassword = new JPasswordField( 20 );
+		appContainer.add( g_lblPassword );
+		appContainer.add( g_fldPassword );
+		
+		// create the Sign In button and add to applet
+		g_btnSignIn = new JButton( "Sign In" );
+		g_btnSignIn.addActionListener( this );
+		appContainer.add( g_btnSignIn );		
+		
+	}// end init()
 
-		// Build String with user's information
-		strConfirmationMessage = "Welcome to " + strApplicationName + " " + strEmail + ". We hope you're performing well!";
-		
-		// Display confirmation message to user
-		JOptionPane.showMessageDialog(null, strConfirmationMessage, "GoalsUp! - going above and beyond schooling!", JOptionPane.PLAIN_MESSAGE);
+	// action to perform when Sign In button is pressed by user
+	public void actionPerformed( ActionEvent actionEvent ){
 
-		System.exit(0);		// terminate application
+		// declare/instantiate local variables
+		String strEmail = g_fldEmail.getText();
+		String strPassword = g_fldPassword.getText();		// this is ONLY for testing and should be removed.
 		
-	}// end main()
+		// display confirmation message
+		JOptionPane.showMessageDialog( null 
+				, "Welcome " + strEmail + " to " + g_strApplicationName 
+				+ "\nWe recommend a stronger password than *" + strPassword 
+				+ "* to increase your security."
+				, "GoalsUp! - going above and beyond schooling!"
+				, JOptionPane.PLAIN_MESSAGE );
+
+	}// end actionPerformed()
 	
 }// end class Main
